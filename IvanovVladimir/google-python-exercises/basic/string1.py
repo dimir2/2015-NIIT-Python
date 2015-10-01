6043bdc353e5f79d755a9e871e621e1bcddfc112
Natalia Rukshina <sunflower9540@gmail.com>
2015-10-01 18:12:59 +0300

    String 1, 2

--- IvanovVladimir/google-python-exercises/basic/string1.py
+++ IvanovVladimir/google-python-exercises/basic/string1.py
@@ -15,7 +15,6 @@
 # It's ok if you do not complete all the functions, and there
 # are some additional functions to try in string2.py.
 
-
 # A. donuts
 # Given an int count of a number of donuts, return a string
 # of the form 'Number of donuts: <count>', where <count> is the number
@@ -24,8 +23,9 @@
 # So donuts(5) returns 'Number of donuts: 5'
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
-  # +++your code here+++
-  return
+  if count >= 10:
+    return 'Number of donuts: many'
+  return 'Number of donuts: ' + str(count)
 
 
 # B. both_ends
@@ -34,8 +34,9 @@ def donuts(count):
 # so 'spring' yields 'spng'. However, if the string length
 # is less than 2, return instead the empty string.
 def both_ends(s):
-  # +++your code here+++
-  return
+  if len(s) < 2:
+    return ''
+  return s[:2] + s[-2:]
 
 
 # C. fix_start
@@ -49,7 +50,7 @@ def both_ends(s):
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
   # +++your code here+++
-  return
+  return s[0] + s[1:].replace(s[0],'*')
 
 
 # D. MixUp
@@ -61,7 +62,7 @@ def fix_start(s):
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
   # +++your code here+++
-  return
+  return b[:2]+a[2:]+' '+a[:2]+b[2:]
 
 
 # Provided simple test() function used in main() to print
